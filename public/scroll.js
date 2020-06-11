35f379a894661307557e0634a2e7f6be12a7c2a4
gueye <gueyemalick125@gmail.com>
2020-06-12 01:57:54 +0200

    first commit

---
+++ public/scroll.js
@@ -0,0 +1,81 @@
+
+const sr = ScrollReveal();
+sr.reveal('h1' , {
+    duration: 1000,
+    origin : 'left',
+    reset : true,
+    distance: '100px'
+});
+
+sr.reveal('.img_designer' , {
+    duration: 2000,
+    origin: 'left',
+    reset: true,
+    distance: '100px'
+});
+sr.reveal('.img_coder' , {
+    duration: 2000,
+    origin: 'right',
+    reset: true,
+    distance: '100px'
+});
+sr.reveal('.content_img_designer' , {
+    duration: 2500,
+    origin: 'bottom',
+    reset: true,
+    distance: '100px',
+    delay : 200
+});
+sr.reveal('.content_img_coder' , {
+    duration: 2500,
+    origin: 'bottom',
+    reset: true,
+    distance: '100px',
+    delay : 200
+});
+sr.reveal('.malick_title' , {
+    duration: 1000,
+    origin: 'top',
+    reset: true,
+    distance: '50px',
+    delay : 200
+});
+sr.reveal('.content_me' , {
+
+    origin: 'left',
+    reset: false,
+    distance: '100px',
+    delay : 250
+});
+sr.reveal('.title_me' , {
+
+    origin: 'right',
+    reset: false,
+    distance: '100px',
+    delay : 250
+});
+sr.reveal('.competence' , {
+    duration: 500,
+    origin: 'left',
+    reset: false,
+    distance: '100px',
+    delay : 200
+});sr.reveal('.competence2' , {
+    duration: 500,
+    origin: 'right',
+    reset: false,
+    distance: '100px',
+    delay : 200
+});
+sr.reveal('.card' , {
+    duration: 1500,
+    origin: 'top',
+    reset: false,
+    distance: '50px',
+
+});
+sr.reveal('.skill' , {
+    duration :1500,
+    origin :'right'
+},);
+
